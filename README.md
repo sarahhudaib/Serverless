9dd69be7666408beec01763570ca5ae5230cbfb6
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-04-11 13:00:55 +0300

    adding 100% solution

---
+++ README.md
@@ -0,0 +1,14 @@
+# Serverless API 
+this app will show the path of whatever the user typed in the browser
+
+## Methods
+- `do_GET(self)` will handle GET requests and send back the response
+- `main ()` this function will start the server and listen for requests
+
+## Result
+
+### from vercel side 
+![alt text](./api/serverless_api.png "serverless_api")
+
+### [Vercel Link](https://serverless-k6efu6whu-sarahhudaib.vercel.app/api/api)
+
--- api/api.py
+++ api/api.py
@@ -3,17 +3,19 @@ from http.server import BaseHTTPRequestHandler , HTTPServer
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
+    """ will handle GET requests and send back the response """
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(self.path[1:].encode())
+    self.wfile.write(self.path[1:].encode()) # will show the path of whatever the user typed in the browser
     return
 
 def main ():
+    """ this function will start the server and listen for requests """
     port = 8080
     server = HTTPServer(('', port), handler)
     print('Started httpserver on port {}'.format(port))
-    server.serve_forever()
+    server.serve_forever() # will run forever until stopped by user (ctrl+c ) in the terminal
 
 
 if __name__ == '__main__':
