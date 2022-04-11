98631452dcc47879764ea064b2195bdd981deb0c
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-04-11 13:39:44 +0300

    adding test new stuff

--- api/api.py
+++ api/api.py
@@ -8,22 +8,24 @@ class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     """ will handle GET requests and send back the response """
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(self.path[1:].encode()) # will show the path of whatever the user typed in the browser
+    if self.path.endswith('/tasklist'):
+      self.send_response(200)
+      self.send_header('Content-type', 'text/plain')
+      self.end_headers()
+      self.wfile.write(self.path[1:].encode()) # will show the path of whatever the user typed in the browser
+      
+      output = ""
+      output += '<html><body>'
+      output += '<h2>Task List</h2>'
+      output+= '<h3> <a href="/tasklist/new"> Add New Task </a> </h3>'
+      for task in tasklist:
+        output+= task
+        output+= '</br>'
+      output += '</body></html>'
+      self.wfile.write(output.encode())
     
-    output = ""
-    output += "<html><body>"
-    output += "<h2>Task List</h2>"
-    for task in tasklist:
-      output+= task
-      output+= "</br>"
-    output += "</body></html>"
-    self.wfile.write(output.encode())
-    print(output)
     
-    return
+    
 
 def main ():
     """ this function will start the server and listen for requests """
