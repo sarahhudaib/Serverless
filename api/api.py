3bbef3be20eb8cbde3cd5cef4ccf26a83f91f824
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-04-11 13:45:46 +0300

    adding test new stuff

--- api/api.py
+++ api/api.py
@@ -4,19 +4,18 @@ from http.server import BaseHTTPRequestHandler , HTTPServer
 tasklist = ['Task 1', 'Task 2', 'Task 3', 'Task 4']
 
 
-class handler(BaseHTTPRequestHandler):
+class requestHandler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     """ will handle GET requests and send back the response """
     if self.path.endswith('/tasklist'):
       self.send_response(200)
-      self.send_header('Content-type', 'text/plain')
+      self.send_header('Content-type', 'text/html')
       self.end_headers()
-      self.wfile.write(self.path[1:].encode()) # will show the path of whatever the user typed in the browser
-      
-      output = ""
+
+      output = ''
       output += '<html><body>'
-      output += '<h2>Task List</h2>'
+      output += '<h1>Task List</h1>'
       output+= '<h3> <a href="/tasklist/new"> Add New Task </a> </h3>'
       for task in tasklist:
         output+= task
@@ -29,9 +28,10 @@ class handler(BaseHTTPRequestHandler):
 
 def main ():
     """ this function will start the server and listen for requests """
-    port = 8080
-    server = HTTPServer(('', port), handler)
-    print('Started httpserver on port {}'.format(port))
+    PORT = 8080
+    server_address = ('localhost', PORT)
+    server = HTTPServer(server_address, requestHandler)
+    print('Started httpserver on port %s'% PORT)
     server.serve_forever() # will run forever until stopped by user (ctrl+c ) in the terminal
 
 
