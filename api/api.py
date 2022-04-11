d6607aee1aa1d1c4e8194e7843bf86dfdc6c64f4
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-04-11 13:56:05 +0300

    adding test new stuff

--- api/api.py
+++ api/api.py
@@ -1,53 +1,21 @@
 from http.server import BaseHTTPRequestHandler , HTTPServer
 
-
-tasklist = ['Task 1', 'Task 2', 'Task 3', 'Task 4']
-
-
-class requestHandler(BaseHTTPRequestHandler):
+class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     """ will handle GET requests and send back the response """
-    if self.path.endswith('/tasklist'):
-      self.send_response(200)
-      self.send_header('Content-type', 'text/html')
-      self.end_headers()
-
-      output = ''
-      output += '<html><body>'
-      output += '<h1>Task List</h1>'
-      output+= '<h3> <a href="/tasklist/new"> Add New Task </a> </h3>'
-      for task in tasklist:
-        output+= task
-        output+= '</br>'
-      output += '</body></html>'
-      
-      self.wfile.write(output.encode())
-   
-    if self.path.endswith('/tasklist/new'):
-      self.send_response(200)
-      self.send_header('Content-type', 'text/html')
-      self.end_headers()
-
-      output = ''
-      output += '<html><body>'
-      output += '<h1>Add a new task</h1>'
-      output += '<form method="POST" enctype="multipart/form-data" action="/tasklist/new">'
-      output += '<input name="task" type="text" placeholder="Add a new task">'
-      output += '<input type="submit" value="Submit">'
-      output += '</form>'
-      output += '</body></html>'
-      
-      self.wfile.write(output.encode())
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(self.path[1:].encode()) # will show the path of whatever the user typed in the browser
     
-    
+    return
 
 def main ():
     """ this function will start the server and listen for requests """
-    PORT = 8080
-    server_address = ('localhost', PORT)
-    server = HTTPServer(server_address, requestHandler)
-    print('Started httpserver on port %s'% PORT)
+    port = 8080
+    server = HTTPServer(('', port), handler)
+    print('Started httpserver on port {}'.format(port))
     server.serve_forever() # will run forever until stopped by user (ctrl+c ) in the terminal
 
 
---
+++ api/get_post.py
@@ -0,0 +1,55 @@
+from http.server import BaseHTTPRequestHandler , HTTPServer
+
+
+tasklist = ['Task 1', 'Task 2', 'Task 3', 'Task 4']
+
+
+class requestHandler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    """ will handle GET requests and send back the response """
+    if self.path.endswith('/tasklist'):
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
+
+      output = ''
+      output += '<html><body>'
+      output += '<h1>Task List</h1>'
+      output+= '<h3> <a href="/tasklist/new"> Add New Task </a> </h3>'
+      for task in tasklist:
+        output+= task
+        output+= '</br>'
+      output += '</body></html>'
+      
+      self.wfile.write(output.encode())
+   
+    if self.path.endswith('/tasklist/new'):
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
+
+      output = ''
+      output += '<html><body>'
+      output += '<h1>Add a new task</h1>'
+      output += '<form method="POST" enctype="multipart/form-data" action="/tasklist/new">'
+      output += '<input name="task" type="text" placeholder="Add a new task">'
+      output += '<input type="submit" value="Submit">'
+      output += '</form>'
+      output += '</body></html>'
+      
+      self.wfile.write(output.encode())
+    
+    return
+
+def main ():
+    """ this function will start the server and listen for requests """
+    PORT = 8080
+    server_address = ('localhost', PORT)
+    server = HTTPServer(server_address, requestHandler)
+    print('Started httpserver on port %s'% PORT)
+    server.serve_forever() # will run forever until stopped by user (ctrl+c ) in the terminal
+
+
+if __name__ == '__main__':
+    main()
